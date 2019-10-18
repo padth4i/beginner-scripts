5ea1c307efd2516646ac21135b47175c2c4c816a
Glenn Paquette <glenn@atomicgrowth.co>
2019-10-18 11:22:47 -0400

    Converted scripts to python 3 & fixed and improved Hangman
    All three scripts are now in python3. I fixed an error and hangman and improved the game by adding a random word generator, and missed guess logger.
    For the random word generation I imported random-word library. I included a requirements file to bring in the library for you.

--- guessinggame.py
+++ guessinggame.py
@@ -1,15 +1,15 @@
 import random
 n = random.randint(1, 99)
-guess = int(raw_input("Enter an integer from 1 to 99: "))
+guess = int(input("Enter an integer from 1 to 99: "))
 while n != "guess":
-    print
+    print()
     if guess < n:
-        print "guess is low"
-        guess = int(raw_input("Enter an integer from 1 to 99: "))
+        print("guess is low")
+        guess = int(input("Enter an integer from 1 to 99: "))
     elif guess > n:
-        print "guess is high"
-        guess = int(raw_input("Enter an integer from 1 to 99: "))
+        print("guess is high")
+        guess = int(input("Enter an integer from 1 to 99: "))
     else:
-        print "you guessed it!"
+        print("you guessed it!")
         break
 
